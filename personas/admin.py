2bfb973e18001350a0efce072d92c7d0b5a84c74
Rafael Ferrero <rafael.ferrero@gmail.com>
2016-06-14 21:59:36 -0300

    Modificamos la clase persona y agregamos admin

--- personas/admin.py
+++ personas/admin.py
@@ -1,2 +1,29 @@
 from django.contrib import admin
+from personas.models import Persona
 
+
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    actions_on_bottom = True
+    list_display = (
+        'apellido',
+        'nombre',
+        'tipo_documento',
+        'documento',
+        'fecha_nacimiento')
+    search_fields = (
+        'apellido',
+        'nombre',
+        'documento',)
+    list_filter = (
+        'apellido',
+        'fecha_nacimiento',
+        'tipo_documento')
+    date_hierarchy = 'fecha_nacimiento'
+    exclude = (
+        'borrado',
+        'historico',
+        'usuario_creador',
+        'fecha_creacion',
+        'usuario_modificador',
+        'fecha_modificacion')
