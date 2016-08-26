3ebbfc4eb14bb52a009852cede40eb33af2c61ae
Rafael Ferrero <rafael.ferrero@gmail.com>
2016-08-26 12:32:39 -0300

    Travis pincha, agrego un par de test mas

--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
-import envdir
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sigcaw.settings")
 
     from django.core.management import execute_from_command_line
 
-    envdir.open()
+    try:
+        import envdir
+        envdir.open()
+    except:
+        pass
 
     execute_from_command_line(sys.argv)
--- personas/tests.py
+++ personas/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
 from tipos_documento.models import TipoDocumento
+from localidades.models import (
+    Localidad,
+    Provincia,
+    Pais)
 from personas.models import (
     Persona,
     Bombero,
@@ -9,10 +13,10 @@ from personas.models import (
 class SimpleTest(TestCase):
 
     def setUp(self):
-
         TipoDocumento.objects.create(
             tipo='Documento',
-            abreviatura='D')
+            abreviatura='D',
+        )
 
         Persona.objects.create(
             apellido='Apellido',
@@ -21,13 +25,40 @@ class SimpleTest(TestCase):
             grupo_sanguineo='O',
             factor_sanguineo='-',
             fecha_nacimiento='1991-01-31',
-            tipo_documento=TipoDocumento.objects.get(abreviatura='D'))
+            tipo_documento=TipoDocumento.objects.get(abreviatura='D'),
+        )
+
+        Pais.objects.create(
+            nombre='Argentina',
+            abreviatura='Arg.',
+        )
+
+        Provincia.objects.create(
+            nombre='Córdoba',
+            abreviatura='Cba.',
+            pais=Pais.objects.get(nombre='Argentina'),
+        )
+
+        Localidad.objects.create(
+            nombre='San Francisco',
+            abreviatura='San Fco.',
+            codigo_postal='2400',
+            provincia=Provincia.objects.get(nombre='Córdoba'),
+        )
 
 
     def test_model_persona(self):
-        persona1 = Persona.objects.get(apellido='Apellido')
-        self.assertEqual(persona1.nombre_completo, 'APELLIDO, Nombre')
-        self.assertEqual(persona1.edad, 25)
-        self.assertEqual(persona1.dni, 'D 12345678')
-        self.assertEqual(persona1.sangre, 'O (-)')
-        self.assertEqual(persona1.aniversario, None)
+        persona = Persona.objects.get(apellido='Apellido')
+        self.assertEqual(persona.nombre_completo, 'APELLIDO, Nombre')
+        self.assertEqual(persona.edad, 25)
+        self.assertEqual(persona.dni, 'D 12345678')
+        self.assertEqual(persona.sangre, 'O (-)')
+        self.assertEqual(persona.aniversario, None)
+
+    def test_model_localidad(self):
+        provincia = Provincia.objects.get(nombre='Córdoba')
+        self.assertEqual(provincia.__str__(), 'Córdoba (Arg.)')
+        localidad = Localidad.objects.get(nombre='San Francisco')
+        self.assertEqual(localidad.__str__(), 'San Francisco, Córdoba (Arg.)')
+        self.assertEqual(localidad.nombre_completo, '(2400) San Francisco, Córdoba (Arg.)')
+
