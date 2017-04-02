ffddb874f31dbf37ab13cd6b6c3e0ced5bda40ec
loki <loki.arg@gmail.com>
2017-04-02 18:02:16 -0300

    agrege todos los campos que charle con @Cristian Carle sobre la info  que tiene que tener el encabezado de un acta.

--- actas/models.py
+++ actas/models.py
@@ -2,11 +2,20 @@ from django.db import models
 
 
 class Acta (models.Model):
+
+    fecha_acta = models.DateField(
+        verbose_name=_('Fecha de Acta'),
+    )
     numero_acta = models.CharField(
         max_length=100,
         verbose_name=_('Numero de Acta'),
         unique=True)
-    fecha_acta = models.DateField(
-        verbose_name=_('Fecha de Acta'),
+    numero_folio = models.CharField(
+        verbose_name=_ ('Numero de Folio'),
+        unique=True,
     )
+    numero_libro = models.CharField(
+        verbose_name=_('Numero de Libro'),
+    )
+    descripcion_acta =_('Descripcion del Acta')
 # Create your models here.
