251054244095dfbd2b6e33578d962393fbb582f6
loki <loki.arg@gmail.com>
2017-04-02 18:00:29 -0300

    agrege el codigo en el admin de actas y creer la clase ActasAdmin.. no pude hacer andar el from de traduccion. investigar.

--- actas/admin.py
+++ actas/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from Actas.models import (
+    Acta,
+)
 
 # Register your models here.
+
+
+@admin.register(Acta)
+class ActaAdmin(admin.ModelAdmin):
+    actions_on_bottom = True
+    fieldsets = (
+        (None, {
+            'fields': (
+                'Numero_acta',
+                'fecha_acta',
+                'numero_folio',
+                'numero_libro',
+                'descripcion_acta',
+                )
+        }),
+    )
+    list_display = (
+
+            'Numero_acta',
+            'fecha_acta',
+            'numero_folio',
+            'numero_libro',
+            'descripcion_acta',
+            )
+    search_fields = (
+            'Numero_acta',
+            'fecha_acta',
+            'numero_folio',
+            'numero_libro',
+            'descripcion_acta',
+            )
+#agregar el campo descripcion_acta.
