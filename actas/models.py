0f1e1e6ea305aca2c352d4b4c9eb5abbeb272a16
loki <loki.arg@gmail.com>
2017-04-02 18:55:23 -0300

    corregi los errores para poder hacer el make migrations . tambien agrege la cantidad de caracteres a los campos que faltaba y  quedo en definir cuantos caracteres debe tener el campo descripcion_acta..

--- actas/models.py
+++ actas/models.py
@@ -1,21 +1,27 @@
 from django.db import models
-
+from django.utils.translation import ugettext as _
 
 class Acta (models.Model):
 
     fecha_acta = models.DateField(
-        verbose_name=_('Fecha de Acta'),
+        verbose_name=('Fecha de Acta'),
     )
     numero_acta = models.CharField(
         max_length=100,
         verbose_name=_('Numero de Acta'),
         unique=True)
     numero_folio = models.CharField(
-        verbose_name=_ ('Numero de Folio'),
+        max_length=100,
+        verbose_name=_('Numero de Folio'),
         unique=True,
     )
     numero_libro = models.CharField(
+        max_length=100,
         verbose_name=_('Numero de Libro'),
     )
-    descripcion_acta =_('Descripcion del Acta')
+    descripcion_acta = models.CharField(
+        verbose_name=_('Descripcion del Acta'),
+        max_length = 100,
+    )
+
 # Create your models here.
