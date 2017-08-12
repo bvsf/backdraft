7469c91daf60182e4420554fea92727a66021d73
Rafael E. Ferrero <rafael.ferrero@gmail.com>
2017-08-12 18:26:56 -0300

    agrego el dumpdata de los grados y las actas de licencias

--- actas/admin.py
+++ actas/admin.py
@@ -1,40 +1,53 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from actas.models import (
-    Acta,
+    Licencia,
 )
 
 
-'''
-El acta es abstracto, se debe poner solo las actas especificas
-
-@admin.register(Acta)
-class ActaAdmin(admin.ModelAdmin):
+@admin.register(Licencia)
+class LicenciaAdmin(admin.ModelAdmin):
     actions_on_bottom = True
     fieldsets = (
         (None, {
             'fields': (
+                'numero_libro',
+                'numero_folio',
                 'numero_acta',
                 'fecha_acta',
-                'numero_folio',
-                'numero_libro',
                 'descripcion_acta',
+                'fecha_desde',
+                'fecha_hasta',
+                'bombero',
                 )
         }),
     )
     list_display = (
-
-            'numero_acta',
+            'acta',
             'fecha_acta',
-            'numero_folio',
-            'numero_libro',
-            'descripcion_acta',
-            )
+            'periodo_licencia',
+            'bombero',
+    )
+    list_filter = (
+            'fecha_acta',
+            'fecha_desde',
+            'fecha_hasta',
+            'bombero',
+    )
     search_fields = (
+            'numero_libro',
+            'numero_folio',
             'numero_acta',
             'fecha_acta',
-            'numero_folio',
-            'numero_libro',
             'descripcion_acta',
-            )
-#agregar el campo descripcion_acta.
-'''
+            'fecha_desde',
+            'fecha_hasta',
+    )
+
+    def acta(self, obj):
+        return obj.nombre_corto
+    acta.short_description = _('Acta')
+
+    def periodo_licencia(self, obj):
+        return obj.periodo_licencia
+    periodo_licencia.short_description = _('Licencia')
--- actas/models.py
+++ actas/models.py
@@ -1,31 +1,73 @@
 from django.db import models
 from django.utils.translation import ugettext as _
+from django.utils import timezone
+from personas.models import Bombero
 
 
 class Acta(models.Model):
     numero_libro = models.CharField(
         max_length=10,
-        verbose_name=_("Numero de Libro"))
+        verbose_name=_("Número de Libro"))
     numero_folio = models.CharField(
         max_length=10,
-        verbose_name=_("Numero de Folio"),
+        verbose_name=_("Número de Folio"),
         unique=True)
     numero_acta = models.CharField(
         max_length=10,
-        verbose_name=_("Numero de Acta"),
+        verbose_name=_("Número de Acta"),
         unique=True)
     fecha_acta = models.DateField(
-        verbose_name=_("Fecha de Acta"),)
+        verbose_name=_("Fecha de Acta"),
+        default=timezone.now)
     descripcion_acta = models.CharField(
-        verbose_name=_("Descripcion del Acta"),
+        verbose_name=_("Descripción del Acta"),
         max_length=1000)
 
     class Meta:
-           abstract = True
+        abstract = True
 
-    def __str__(self):
-        return "Libro: {0} Folio: {1} Acta: {2} - Fecha:".format(
+    @property
+    def nombre_completo(self):
+        return _("Libro: {0} Folio: {1} Acta: {2} - Fecha:").format(
             self.numero_libro,
             self.numero_folio,
             self.numero_acta,
             self.fecha_acta)
+
+    @property
+    def nombre_corto(self):
+        return _("L.{0} / F.{1} / A.{2}").format(
+            self.numero_libro,
+            self.numero_folio,
+            self.numero_acta,
+        )
+
+    def __str__(self):
+        return "{0}".format(self.nombre_completo)
+
+
+class Licencia(Acta):
+    fecha_desde = models.DateField(
+        verbose_name=_("Fecha desde")
+    )
+    fecha_hasta = models.DateField(
+        verbose_name=_("Fecha hasta")
+    )
+    bombero = models.ForeignKey(
+        Bombero,
+        verbose_name=_("Bombero")
+    )
+
+    def __str__(self):
+        return _("({0}) - Licencia desde {1} hasta {2}").format(
+            self.nombre_corto,
+            self.fecha_acta,
+            self.fecha_hasta
+        )
+
+    @property
+    def periodo_licencia(self):
+        return _("Desde: {0} hasta: {1}").format(
+            self.fecha_desde,
+            self.fecha_hasta,
+        )
