044d2d858038f1fea9ab24158b28cff81e1c0edd
Rafael E. Ferrero <rafael.ferrero@gmail.com>
2017-11-11 19:03:03 -0300

    más PEP8 los serializers

--- localidades/api/views.py
+++ localidades/api/views.py
@@ -5,27 +5,27 @@ from localidades.api.serializers import LocalidadSerializer, ProvinciaSerializer
 
 '''
 class ZonaListAPIView(ListAPIView):
-	queryset = Zona.objects.all()
-	serializer_class = BomberoSerializer
+    queryset = Zona.objects.all()
+    serializer_class = BomberoSerializer
 
-	#def get_queryset()
+    #def get_queryset()
 '''
 
 class LocalidadListAPIView(ListAPIView):
-	queryset = Localidad.objects.all()
-	serializer_class = LocalidadSerializer
+    queryset = Localidad.objects.all()
+    serializer_class = LocalidadSerializer
 
 
 class ProvinciaListAPIView(ListAPIView):
-	queryset = Provincia.objects.all()
-	serializer_class = ProvinciaSerializer
+    queryset = Provincia.objects.all()
+    serializer_class = ProvinciaSerializer
 
 class LocalidadDetailAPIView(RetrieveAPIView):
-	queryset = Localidad.objects.all()
-	serializer_class = LocalidadSerializer
-	lookup_field = 'pk'
+    queryset = Localidad.objects.all()
+    serializer_class = LocalidadSerializer
+    lookup_field = 'pk'
 
 class ProvinciaDetailAPIView(RetrieveAPIView):
-	queryset = Provincia.objects.all()
-	serializer_class = ProvinciaSerializer
-	lookup_field = 'pk'
+    queryset = Provincia.objects.all()
+    serializer_class = ProvinciaSerializer
+    lookup_field = 'pk'
--- personas/api/urls.py
+++ personas/api/urls.py
@@ -2,15 +2,14 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from personas.api.views import (
-	PersonaListAPIView,
-	BomberoListAPIView,
-	BomberoDetailAPIView,
-	BomberoUpdateAPIView,
-	)
+    PersonaListAPIView,
+    BomberoListAPIView,
+    BomberoDetailAPIView,
+    BomberoUpdateAPIView,
+    )
 
 urlpatterns = [
-	url(r'^listado/bomberos$', BomberoListAPIView.as_view(), name='firefighter-list'),
-	url(r'^detalle/bombero/$', BomberoDetailAPIView.as_view(), name='firefighter'),
-	url(r'^listado/personas$', PersonaListAPIView.as_view(), name='people-list'),
-	
+    url(r'^listado/bomberos$', BomberoListAPIView.as_view(), name='firefighter-list'),
+    url(r'^detalle/bombero/$', BomberoDetailAPIView.as_view(), name='firefighter'),
+    url(r'^listado/personas$', PersonaListAPIView.as_view(), name='people-list'),
 ]
