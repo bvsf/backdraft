49470f31131a867fe7a3ab23e51cc8391d0e4477
Rafael E. Ferrero <rafael.ferrero@gmail.com>
2017-11-13 20:20:26 -0300

    faltaba el id en el endpoint de bomberos

--- personas/api/urls.py
+++ personas/api/urls.py
@@ -10,6 +10,6 @@ from personas.api.views import (
 
 urlpatterns = [
     url(r'^listado/bomberos$', BomberoListAPIView.as_view(), name='firefighter-list'),
-    url(r'^detalle/bombero/$', BomberoDetailAPIView.as_view(), name='firefighter'),
+    url(r'^detalle/bombero/(?P<pk>[0-9]+)/$', BomberoDetailAPIView.as_view(), name='firefighter'),
     url(r'^listado/personas$', PersonaListAPIView.as_view(), name='people-list'),
 ]
