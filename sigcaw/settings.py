36a71920086c6a76f0c20b7e8bcb78b474568a46
Rafael E. Ferrero <rafael.ferrero@gmail.com>
2020-05-16 17:35:30 -0300

    elimino lo que es de docker y actualizo la receta de jenkins

--- sigcaw/settings.py
+++ sigcaw/settings.py
@@ -107,18 +107,6 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', ''),
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'database1',
-        'USER': 'database1_role',
-        'PASSWORD': 'database1_password',
-        'HOST': 'database1',
-        'PORT': '5432',
-    }
-}
-'''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
