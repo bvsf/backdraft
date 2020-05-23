c091358666819e595cfbce1fa4476a86ca315158
Rafael E. Ferrero <rafael.ferrero@gmail.com>
2020-05-23 18:40:36 -0300

    Falló script deploy Falló el script de deploy por falta de permisos en github

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -2,7 +2,6 @@
 
 # https://rahmonov.me/posts/continuous-integration-and-continous-deployment-for-django-app-with-jenkins/
 
-ssh root@srv-backdraft <<EOF
   cd backdraft
   git pull .
   source /backdraft/venv/bin/activate
@@ -10,4 +9,3 @@ ssh root@srv-backdraft <<EOF
   ./manage.py migrate
   sudo supervisorctl restart backdraft
   exit
-EOF
